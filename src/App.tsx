68fbbab61257e06fcfa7a0fdceec86901e38e124
Harits <harits.abdullah19@gmail.com>
2024-12-19 11:11:21 +0700

    add is scrolled event for header

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,41 @@
+import { useRef, useState } from "react";
 import AboutView from "./component/about-view";
 import HeaderView from "./component/header-view";
 import { useThemeState } from "./state/theme_state";
+import { ScrollContext } from "./state/scroll_state";
 
 function App() {
   const { mode } = useThemeState();
+  const [scrollTop, setScrollTop] = useState(0);
+  const isScrolled = scrollTop > 0;
+  const ref = useRef<HTMLDivElement>(null);
   return (
-    <div className={`${mode === "dark" ? "dark " : "light"} h-screen w-screen`}>
+    <ScrollContext.Provider value={isScrolled}>
       <div
-        className={`h-screen w-screen flex flex-col overflow-y-scroll font-inter no-scrollbar bg-light-gray-default dark:bg-dark-gray-default text-dark-gray-default dark:text-light-gray-default`}
+        className={`${mode === "dark" ? "dark " : "light"} h-screen w-screen`}
       >
-        <div className="h-full overflow-y-scroll w-full no-scrollbar">
-          <HeaderView />
-          <AboutView />
-          <div className="h-[10000px]" />
+        <div
+          className={`h-screen w-screen flex flex-col overflow-y-scroll font-inter no-scrollbar bg-light-gray-default dark:bg-dark-gray-default text-dark-gray-default dark:text-light-gray-default`}
+        >
+          <div className="fixed top-0 w-full">
+            <HeaderView />
+          </div>
+          <div
+            ref={ref}
+            className="h-full overflow-y-scroll w-full no-scrollbar"
+            onScroll={() => {
+              setScrollTop(ref.current?.scrollTop ?? 0);
+            }}
+          >
+            <div className="opacity-0 pointer-events-none">
+              <HeaderView />
+            </div>
+            <AboutView />
+            <div className="h-[10000px]" />
+          </div>
         </div>
       </div>
-    </div>
+    </ScrollContext.Provider>
   );
 }
 
--- src/component/header-view/index.tsx
+++ src/component/header-view/index.tsx
@@ -1,4 +1,5 @@
 import assets from "../../constan/assets";
+import { useScrollState } from "../../state/scroll_state";
 import { useThemeState } from "../../state/theme_state";
 import Button from "../button";
 import Divider from "../divider";
@@ -7,6 +8,8 @@ import Spacer from "../spacer";
 export default function HeaderView() {
   const { mode, setMode } = useThemeState();
 
+  const isScrolled = useScrollState();
+
   const openNewTab = (link: string) => {
     window.open(link, "_blank");
   };
@@ -14,10 +17,16 @@ export default function HeaderView() {
   const baseUrl =
     "https://docs.google.com/document/d/1sG-A9ulHr12cY02n_0s_DS6hUrgpGsopaEGPfsZP0uo";
   return (
-    <div className="px-20 py-4 flex flex-row items-center">
+    <div
+      className={`px-20 py-4 flex flex-row items-center   ${
+        isScrolled ? "backdrop-blur-xl border-light-gray-100 dark:border-dark-gray-100 bg-light-gray-50 dark:bg-dark-gray-50" : ""
+      }`}
+    >
       <div className="font-bold text-[30px]">{"<AbdullahHarits/>"}</div>
       <Spacer />
-      <div className={`flex flex-row gap-x-6 font-medium text-base items-center `}>
+      <div
+        className={`flex flex-row gap-x-6 font-medium text-base items-center `}
+      >
         {["About", "Work", "Testimonials", "Contact"].map((item) => (
           <div
             key={item}
