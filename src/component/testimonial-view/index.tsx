7193515d2a9d3cc2e71d3df3aa6121f26c0a2ea9
Harits <harits.abdullah19@gmail.com>
2024-12-30 10:48:56 +0700

    implement testimonial view

--- src/component/testimonial-view/index.tsx
+++ src/component/testimonial-view/index.tsx
@@ -1,3 +1,33 @@
+import { testimonials } from "../../constan/testimonials";
+import DetailView from "../detail-view";
+
 export default function TestimonialView() {
-  return <div>Testimonial View</div>;
+  return (
+    <DetailView
+      title="Testimonials"
+      desc="Nice things people have said about me:"
+    >
+      <div className="mt-12 flex flex-row justify-center items-center gap-x-12">
+        {testimonials.map(({ desc, image, name, relation }) => (
+          <div className="flex flex-col items-center p-12 rounded-xl shadow-md gap-y-6 h-[428px] bg-light-gray-default dark:bg-dark-gray-default">
+            <img
+              src={image}
+              className="h-16 w-16 object-cover rounded-full overflow-hidden"
+            />
+            <span className="text-body2 text-light-gray-600 dark:text-dark-gray-600 ">
+              {desc}
+            </span>
+            <div className="flex flex-col items-center">
+              <span className="text-subtitle-desktop font-semibold text-light-gray-900 dark:text-dark-gray-900">
+                {name}
+              </span>
+              <span className="text-body3 text-light-gray-600 dark:text-dark-gray-600">
+                {relation}
+              </span>
+            </div>
+          </div>
+        ))}
+      </div>
+    </DetailView>
+  );
 }
---
+++ src/constan/testimonials.tsx
@@ -0,0 +1,28 @@
+import assets from "./assets";
+
+export const testimonials = [
+  {
+    name: "John Doe",
+    image: assets.image.profileSquare,
+    desc: `“Job well done! I am really impressed. He is very very good at
+              what he does:) I would recommend Sagar and will rehire in the
+              future for Frontend development.”`,
+    relation: "Founder - xyz.com",
+  },
+  {
+    name: "John Doe",
+    image: assets.image.profileSquare,
+    desc: `“Job well done! I am really impressed. He is very very good at
+              what he does:) I would recommend Sagar and will rehire in the
+              future for Frontend development.”`,
+    relation: "Founder - xyz.com",
+  },
+  {
+    name: "John Doe",
+    image: assets.image.profileSquare,
+    desc: `“Job well done! I am really impressed. He is very very good at
+              what he does:) I would recommend Sagar and will rehire in the
+              future for Frontend development.”`,
+    relation: "Founder - xyz.com",
+  },
+];
